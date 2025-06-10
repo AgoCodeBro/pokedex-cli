b3e266a5fc0e6a81e065c38c556113c5d73d185a
Agostino <agoimbimboparra@gmail.com>
2025-06-10 14:00:41 -0400

    Added map and mapb commands

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -1,3 +1,52 @@
 package pokeapi
 
+import ("fmt"
+        "encoding/json"
+				"net/http"
+				"errors"
+        )
 
+type NamedAPIResource struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type ResourceList struct {
+	Count    int    `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string    `json:"previous"`
+	Results  []NamedAPIResource `json:"results"`
+}
+
+
+func GetAPIPage(url string) (ResourceList, error) {
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		errString := fmt.Sprintf("Failed to create request: %v", err)
+		return ResourceList{}, errors.New(errString)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		errString := fmt.Sprintf("Failed to get request: %v", err)
+		return ResourceList{}, errors.New(errString)
+	}
+
+	defer res.Body.Close()
+
+	if statusCode := res.StatusCode; statusCode < 200 || statusCode > 299 {
+		errString := fmt.Sprintf("Error Status Code: v%", statusCode)
+		return ResourceList{}, errors.New(errString)
+	}
+	
+	var result ResourceList
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&result); err != nil {
+		errString := fmt.Sprintf("Failed to decode json: %v")
+		return ResourceList{}, errors.New(errString)
+	}
+
+	return result, nil
+}
--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import ("fmt"
         "bufio"
         "os"
         "errors"
+				"pokedex/internal/pokeapi"
         )
+
+// Config to hold pev and next page
+type config struct {
+	next  *string
+	prev  *string
+}
 
 // Commands
 type cliCommand struct {
     name        string
     description string
-    callback    func() error
+    callback    func(*config) error
 }
 
 var commands = make(map[string]cliCommand)
@@ -27,6 +34,15 @@ func initCommands() {
                                   callback :  commandExit,
                                  }
                                  
+		commands["map"] = cliCommand{name : "map",
+																 description : "Displays the names of 20 location areas. Calling it again will display the next 20. (see mapb)",
+																 callback : commandMap,
+																}
+	
+		commands["mapb"] = cliCommand{name : "mapb",
+																 description : "Displays the previous page of locations",
+																 callback : commandMapb,
+																}
 }
 
 
@@ -34,30 +50,34 @@ func main() {
     scanner := bufio.NewScanner(os.Stdin)
     initCommands()
 
+		initialUrl := "https://pokeapi.co/api/v2/location-area/"
+	  config := config{next : &initialUrl,
+										prev : nil,
+									 }
+
 
     for true {
         fmt.Printf("Pokedex > ")
         scanner.Scan()
         rawText := scanner.Text()
         inputs := cleanInput(rawText)
-        
         val, ok := commands[inputs[0]]
         if !ok {
             fmt.Printf("Not a valid command\n")
         } else {
-            val.callback()
+            val.callback(&config)
         }
     }
 }
 
 // Command Callbacks
-func commandExit() error {
+func commandExit(c *config) error {
     fmt.Printf("Closing the Pokedex... Goodbye!\n")
     os.Exit(0)
     return errors.New("IDK how you got here bruh, failed to exit")
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
     fmt.Printf("Welcome to the Pokedex!\n   Usage:\n\n")
     for _, value := range commands {
         fmt.Printf("%v: %v\n", value.name, value.description)
@@ -65,6 +85,53 @@ func commandHelp() error {
     return nil
 }
 
+func commandMap(c *config) error {
+	if c.next == nil {
+		fmt.Printf("At final page")
+		return nil
+	}
+
+	url := *c.next
+	
+
+	page, err := pokeapi.GetAPIPage(url)
+	if err != nil {
+		return err
+	}
+
+	c.next = page.Next
+	c.prev = page.Previous
+
+	for _, result := range page.Results {
+		fmt.Printf("%v\n", result.Name)
+	}
+	
+	return nil
+
+}
+
+func commandMapb(c *config) error {
+	if c.prev == nil {
+		fmt.Printf("At the first page\n")
+		return nil
+	}
+	url := *c.prev
+	
+	
+	page, err := pokeapi.GetAPIPage(url)
+	if err != nil {
+		return err
+	}
+
+	c.next = page.Next
+	c.prev = page.Previous
+
+	for _, result := range page.Results {
+		fmt.Printf("%v\n", result.Name)
+	}
+
+	return nil
+}
 
 // Other Functions
 func cleanInput(text string) []string {
